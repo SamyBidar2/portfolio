2264e24c58e70411a6ec59629bfea60fabec8c6e
SamyBidar2 <samybidarpro@gmail.com>
2024-05-28 11:40:54 +0200

    Avancement Timeline

--- src/Components/Timeline.jsx
+++ src/Components/Timeline.jsx
@@ -1,21 +1,86 @@
 import React from 'react';
-import { VerticalTimeline,  VerticalTimelineElement} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
-// import styled from 'styled-components';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import styled from 'styled-components';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Container from 'react-bootstrap/Container';
-// import timelineData from '../Datas/timelinedatas.json';
+import Container from 'react-bootstrap/Container';
+import timelineData from '../Datas/timelinedatas.json';
 
+const TimelineContainer = styled(Container)`
+  display: flex;  
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 200px;
+  width: 100%;
+  overflow: hidden;
+  padding-bottom: 10%;    
+`;
+
+const TimelineTitle = styled.h2`
+  padding: 40px 0;
+  text-align: center;
+`;
+
+const StyledVerticalTimelineElement = styled(VerticalTimelineElement)`
+  .vertical-timeline-element-content {
+    border: 1px solid #2b303b;
+    background: #15181e;
+    box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.04), 0px 3px 8px 0px rgba(0, 0, 0, 0.12), 0px 8px 16px 0px rgba(0, 0, 0, 0.60);
+    backdrop-filter: blur(12px);
+  }
+
+  .vertical-timeline-element-content-arrow {
+    border-right-color: #15181e; 
+  }
+
+  .vertical-timeline-element-title {
+    font-size:24px;
+  }
+
+  .vertical-timeline-element-subtitle {
+    font-size:20px;
+  }
+
+  .vertical-timeline-element-text {
+    color: var(--Adaptive-Weak, rgba(255, 255, 255, 0.60));
+    font-family: Inter;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px;
+    margin-bottom: 20px;
+  }
+
+  .vertical-timeline-element-date {
+    color: var(--Adaptive-Weak, rgba(255, 255, 255, 0.60));
+    font-family: Inter;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px;
+    margin-bottom: 20px;
+  }
+`;
 
 export const Timeline = () => {
-return (
-    <VerticalTimeline>
-        <VerticalTimelineElement> Expérience 1 </VerticalTimelineElement>
-        <VerticalTimelineElement> Expérience 2 </VerticalTimelineElement>
-        <VerticalTimelineElement> Expérience 3 </VerticalTimelineElement>
+  return (
+    <TimelineContainer>
+      <TimelineTitle className='text-light'>Expérience</TimelineTitle>
 
-    </VerticalTimeline>
-    );
+      <VerticalTimeline>
+        {timelineData.map((data, index) => (
+          <StyledVerticalTimelineElement key={`${data.id}-${index}`}>
+            <h3 className='vertical-timeline-element-title text-light'>{data.jobtitle}</h3>
+            <h4 className='vertical-timeline-element-subtitle text-light'>{data.company}</h4>
+            
+            <p className='vertical-timeline-element-text text-light'>{data.description}</p>
+            <span className='vertical-timeline-element-date text-light'>{data.date}</span>
+          </StyledVerticalTimelineElement>
+        ))}
+      </VerticalTimeline>
+    </TimelineContainer>
+  );
 };
 
 export default Timeline;
